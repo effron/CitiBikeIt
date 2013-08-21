59b92028415a85cd74798a0d2860279c1d4577ec
Harris Effron <harris.effron@gmail.com>
2013-08-21 15:47:34 -0400

    Initial commit. New empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CitiBikeIt::Application.config.secret_token = '4feac832b258479b5d6c455af11fa894a35837c968d9a3ecd5e119e78c3d96a18ef0d195fe716fbd182b9cdafd7fe880968a92077ed0b5b18c1387d2827a49b4'
